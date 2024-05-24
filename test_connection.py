40b645257d3e96945371964f454211b4d64bd242
Sunil Kandola <sunil.kandola.mba19@said.oxford.edu>
2024-05-24 16:56:11 +0100

    update test file

--- test_connection.py
+++ test_connection.py
@@ -1,9 +1,18 @@
 import requests
+import json
+
+payload = {
+    "jsonrpc": "2.0",
+    "method": "eth_blockNumber",
+    "params": [],
+    "id": 1
+}
 
 try:
-    response = requests.get('http://127.0.0.1:7545')
+    response = requests.post('http://127.0.0.1:7545', json=payload)
     if response.status_code == 200:
         print("Connection to Ganache is successful!")
+        print("Response:", response.json())
     else:
         print(f"Received unexpected status code {response.status_code}")
 except requests.ConnectionError:
